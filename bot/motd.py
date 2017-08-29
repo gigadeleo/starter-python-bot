fc899f8864e762e1bf3b10653124e84d108187de
gigadeleo <gdeleonardo@gmail.com>
2017-08-29 11:00:11 +0200

    no message

--- bot/motd.py
+++ bot/motd.py
@@ -68,14 +68,9 @@ def create_mwotd(maltese_word, english_word, word_type):
     rnd_back = random.randint(1,6)
     img = Image.open("bot/motd/image" + str(rnd_back) + ".png")
     draw = ImageDraw.Draw(img)
-    mword_font = ImageFont.load_default()
-    eword_font = ImageFont.load_default()
-    #mword_font = ImageFont.truetype(font="Verdana.ttf", size=50)
-    #eword_font = ImageFont.truetype(font="Verdana.ttf", size=30)
-    mword_font = ImageFont.truetype(size=50)
-    eword_font = ImageFont.truetype(size=30)
+    mword_font = ImageFont.truetype(font="verdana.ttf", size=50)
+    eword_font = ImageFont.truetype(font="verdana.ttf", size=30)
     
-
     # textwrap long words (chances are it may overlap)
     maltese_wordwrap = textwrap.wrap(maltese_word, width=18)
     english_wordwrap = textwrap.wrap(english_word, width=30)
