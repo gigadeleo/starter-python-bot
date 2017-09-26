9d3519e4c192afb42bcbed1d774ae58bde367a38
Giannella De Leonardo <gdeleonardo@gmail.com>
2017-09-26 20:47:53 +0200

    minor fixes

--- bot/messenger.py
+++ bot/messenger.py
@@ -77,7 +77,7 @@ class Messenger(object):
     def write_motd(self, channel_id, file, example_phrase):
         txt = "Here is the Maltism of the Day (motd)"
         self.send_message(channel_id, txt)
-        self.send_message(channel_id, "iffjaqq")
+        self.post_image(filename=file, token=slack_token, channels=channel_id)
         #self.clients.web.chat.post_image(filename=file, token=slack_token, channels=channel_id)
         self.send_message(channel_id, example_phrase)
 
